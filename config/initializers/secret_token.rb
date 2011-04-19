147429b89482ebbdd80cd0ed07d6956a60474fcd
Dmitry Veremchuk <dmitry.veremchuk@me.com>
2011-04-19 10:55:10 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectShippy::Application.config.secret_token = '9980f7289de25e2a88204866a07c1bdbc3a9a03194e57a92fdd7d0e6446b83f2e8c0cfe485e9e231f9e87ef7906e0f45b9bca572d39fed33d156e9cf66f9fd86'
