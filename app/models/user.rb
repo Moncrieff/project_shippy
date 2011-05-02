03fb9b9d090851cc01effbbf79fd386460cb41db
Moncrieff <dmitry.veremchuk@me.com>
2011-05-02 22:58:53 +0400

    Partly completed user specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,9 @@
 
 class User < ActiveRecord::Base
   
- attr_accessible :name, :email 
+ attr_accessible :name, :email
+ 
+ validates :name, :presence => true
+ validates :email, :presence => true
   
 end
